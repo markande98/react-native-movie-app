31c8f1972c8aa08b686eae92bf06c42cc9d4e127
markande98 <gauravbpit1998@gmail.com>
2025-09-17 17:51:01 +0530

    feat(UI):Home Screen UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,30 @@
-import { Text, View } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
+import { Image, ScrollView, View } from "react-native";
 
 export default function Index() {
+  const router = useRouter();
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-5xl font-bold text-dark-200">Welcome!</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute z-0 w-full" />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          minHeight: "100%",
+          paddingBottom: 10,
+        }}
+      >
+        <Image source={icons.logo} className="mx-auto mb-5 mt-20 h-10 w-12" />
+        <View className="mt-5 flex-1">
+          <SearchBar
+            onPress={() => router.push("/search")}
+            placeholder="Search for a movie"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
---
+++ components/SearchBar.tsx
@@ -0,0 +1,31 @@
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, TextInput, View } from "react-native";
+
+interface Props {
+  placeholder: string;
+  onPress?: () => void;
+}
+
+const SearchBar = ({ onPress, placeholder }: Props) => {
+  return (
+    <View className="flex-row items-center rounded-full bg-dark-200 px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor="#a8b5bd"
+        className="ml-2 flex-1 text-white"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
